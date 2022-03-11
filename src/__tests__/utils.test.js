13a3f8cbfe6f27c2e54da87b383b59ce1dbd9558
jendevelops <jennifer.batara.dev@gmail.com>
2022-03-11 01:09:21 -0800

    Adds error handling for empty inputs

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,6 +1,7 @@
 import { 
+  errorMessages,
   transformTextToLowerCase,
-  transformTextToUpperCase
+  transformTextToUpperCase,
 } from '../utils';
 
 describe('Transform functions tests', () => {
@@ -11,10 +12,22 @@ describe('Transform functions tests', () => {
     expect(outputText).toEqual(expectedOutputText);
   });
 
+  test('transformTextToLowerCase invalid input', () => {
+    const inputText = '';
+    const testFn = () => {transformTextToLowerCase(inputText)};
+    expect(testFn).toThrow(errorMessages.emptyInput);
+  });
+
   test('transformTextToUpperCase valid input', () => {
     const inputText = 'Testing one, two, 3.';
     const outputText = transformTextToUpperCase(inputText);
     const expectedOutputText = 'TESTING ONE, TWO, 3.';
     expect(outputText).toEqual(expectedOutputText);
+  });
+
+  test('transformTextToUpperCase invalid input', () => {
+    const inputText = '';
+    const testFn = () => {transformTextToUpperCase(inputText)};
+    expect(testFn).toThrow(errorMessages.emptyInput);
   });
 })
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,21 @@
-export const transformTextToLowerCase =  (text) => text.toLowerCase();
+export const errorMessages = {
+  emptyInput: 'Input needed to convert text.',
+  default: 'Error occured. Please try again.'
+}
 
-export const transformTextToUpperCase = (text) => text.toUpperCase();
+export const transformTextToLowerCase =  (text) => {
+  if(!text){
+    throw errorMessages.emptyInput;
+  }
+  return text.toLowerCase();
+}
+
+export const transformTextToUpperCase = (text) => {
+  if(!text){
+    throw errorMessages.emptyInput;
+  }
+  return text.toUpperCase();
+}
 
 export const conversionOptions = {
   lowercase: {
